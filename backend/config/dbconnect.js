321f0479b36421a28a2202d429126a70a4f6e5ba
Shivanshu Singh <sr5752@srmist.edu.in>
2025-04-04 10:11:09 +0530

    new

--- backend/config/dbconnect.js
+++ backend/config/dbconnect.js
@@ -13,5 +13,15 @@ const connectDB = async () => {
   }
 };
 
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(`Mongo server run on ${mongoose.connection.host}`.bgGreen);
+  } catch (error) {
+    console.log(`${error}`.bgRed);
+  }
+};
+
 module.exports = connectDB
-// export default connectDB;\\\\
+
